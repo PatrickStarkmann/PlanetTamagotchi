4e45ce7e493a097a2886ae6c564a5080d5df3ef1
Patrick Starkmann <patrick.starkmann@study.hs-duesseldorf.de>
2024-05-11 17:55:41 +0200

    neues Layout 11.05.

--- app/src/main/java/com/example/planettamagotchi/MainActivity.java
+++ app/src/main/java/com/example/planettamagotchi/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.planettamagotchi;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,12 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ProgressBar;
-import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
-    private ProgressBar progressBar;
+    // private ProgressBar progressBar;
     private HealthBar healthBar;
 
     @Override
@@ -22,18 +17,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.zaehler), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        progressBar = findViewById(R.id.progressBar);
-        healthBar = new HealthBar(progressBar);
+       // progressBar = findViewById(R.id.progressBar);
+       // healthBar = new HealthBar(progressBar);
 
-        configureNextButton();
+       // configureNextButton();
     }
     // ShopButton, Von Mainactivity zum Shop
+    /* Erstmal auskommentiert weil ein Fehler angezeigt wurde
     private void configureNextButton() {
         ImageButton shopButton = findViewById(R.id.shopButton);  // Button mit der ID buttonZuMain
         shopButton.setOnClickListener(new View.OnClickListener(){
@@ -43,4 +39,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+     */
+
 }
--- app/src/main/java/com/example/planettamagotchi/Shop.java
+++ app/src/main/java/com/example/planettamagotchi/Shop.java
@@ -10,8 +10,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.example.planettamagotchi.MainActivity;
-
 public class Shop extends AppCompatActivity {
 
     @Override
@@ -19,7 +17,7 @@ public class Shop extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_shop);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.zaehler), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
