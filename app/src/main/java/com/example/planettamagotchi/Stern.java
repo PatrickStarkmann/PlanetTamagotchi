d60c9319a00b6fdbff6a74f5c6305514f4cdff88
Julian <jureich2002@gmail.com>
2024-05-13 18:35:55 +0200

    13.5 stern Klasse verbessert

--- app/src/main/java/com/example/planettamagotchi/Stern.java
+++ app/src/main/java/com/example/planettamagotchi/Stern.java
@@ -4,31 +4,47 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.widget.ImageView;
-
+import android.widget.RelativeLayout;
 import java.util.Random;
 
 public class Stern {
 
     public Bitmap bitmap;
     private int sternX, sternY;
-    Random random;
+    private Random random;
+    private ImageView imageView;
 
     //Konstruktor
-    public Stern(Context context, ImageView imageView){
-        this.bitmap = BitmapFactory.decodeResource(context.getResources(),R.drawable.star);
+    public Stern(Context context, RelativeLayout layout){
+        this.imageView = new ImageView(context);
+        this.bitmap = BitmapFactory.decodeResource(context.getResources(),R.drawable.small_star);
         this.random = new Random();
         this.randomPosition();
-        this.setImageView(imageView);
+        this.setImageView(layout);
     }
 
+    /**
+     * setzt x und y auf random werte
+     */
     public void randomPosition(){
         this.sternX = random.nextInt(1008);
         this.sternY = random.nextInt(1604);
     }
 
-    public void setImageView(ImageView imageView) {
+
+    /**
+     * setzt die Position des Sterns und die ImageView
+     * @param layout = das layout auf welches der Stern abgebildet wird
+     */
+    public void setImageView(RelativeLayout layout) {
         imageView.setImageBitmap(this.bitmap);
+        imageView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        layout.addView(imageView);
+        imageView.setX(sternX);
+        imageView.setY(sternY);
     }
+
+
 
 }
 
