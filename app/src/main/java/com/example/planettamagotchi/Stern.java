21fd75f9500a507469b15078b2eacd4be6e25986
Julian <jureich2002@gmail.com>
2024-05-13 18:49:47 +0200

    13.5 stern Klasse kleinigkeiten verbessert

--- app/src/main/java/com/example/planettamagotchi/Stern.java
+++ app/src/main/java/com/example/planettamagotchi/Stern.java
@@ -9,34 +9,41 @@ import java.util.Random;
 
 public class Stern {
 
-    public Bitmap bitmap;
+    private Bitmap bitmap;
     private int sternX, sternY;
     private Random random;
     private ImageView imageView;
 
     //Konstruktor
-    public Stern(Context context, RelativeLayout layout){
-        this.imageView = new ImageView(context);
-        this.bitmap = BitmapFactory.decodeResource(context.getResources(),R.drawable.small_star);
+    public Stern(Context _context, RelativeLayout _layout){
+        this.setImageView(_context);
+        this.bitmap = BitmapFactory.decodeResource(_context.getResources(),R.drawable.small_star);
         this.random = new Random();
         this.randomPosition();
-        this.setImageView(layout);
+        this.bitmapToImageView(_layout);
     }
 
-    /**
+    /**setImageView
+     * setzt die imageView für den Konstruktor
+     * @param _context = context aus konstruktor
+     */
+    public void setImageView(Context _context){
+        this.imageView = new ImageView(_context);
+    }
+    /**randomPosition
      * setzt x und y auf random werte
      */
     public void randomPosition(){
         this.sternX = random.nextInt(1008);
-        this.sternY = random.nextInt(1604);
+        this.sternY = random.nextInt(1304);
     }
 
 
-    /**
+    /**setImageView
      * setzt die Position des Sterns und die ImageView
      * @param layout = das layout auf welches der Stern abgebildet wird
      */
-    public void setImageView(RelativeLayout layout) {
+    public void bitmapToImageView(RelativeLayout layout) {
         imageView.setImageBitmap(this.bitmap);
         imageView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
         layout.addView(imageView);
