f5139fff2014d94f89ccfde6d5659f09e8c6cee9
Oli <oliver.dick@stud.hn.de>
2024-05-20 00:22:23 +0200

    Tee und Cola kann man testweise kaufen

--- app/src/main/java/com/example/planettamagotchi/Shop.java
+++ app/src/main/java/com/example/planettamagotchi/Shop.java
@@ -1,9 +1,11 @@
-package com.example.planettamagotchi;// Shop.java
+package com.example.planettamagotchi;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +15,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Shop extends AppCompatActivity {
     private ImageView zuMainButton;
+    private TextView colaCounterTextView;
+    private TextView teaCounterTextView;
+    private int colaCounter = 0;
+    private int teaCounter = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_shop);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.zaehler), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -31,8 +39,31 @@ public class Shop extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(Shop.this, MainActivity.class);
                 startActivity(intent);
-                // Optional: Finish the current activity if you don't want to keep it in the back stack
                 finish();
+            }
+        });
+
+        // Counters
+        colaCounterTextView = findViewById(R.id.textView);
+        teaCounterTextView = findViewById(R.id.textView2);
+
+        // Cola kaufen
+        Button colaButton = findViewById(R.id.button);
+        colaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                colaCounter++;
+                colaCounterTextView.setText(String.valueOf(colaCounter));
+            }
+        });
+
+        // Tee kaufen
+        Button teaButton = findViewById(R.id.button2);
+        teaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                teaCounter++;
+                teaCounterTextView.setText(String.valueOf(teaCounter));
             }
         });
     }
